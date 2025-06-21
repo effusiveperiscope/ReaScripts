be0bc1dbd1556c35ad8d016a4e3376abf1d8e984
Anonymous <>
2025-06-21 11:37:12 -0700

    Set inputs actions

---
+++ Set inputs/Set current track to mono input 2.lua
@@ -0,0 +1,12 @@
+selected_count = reaper.CountSelectedTracks(0)
+for i=0, selected_count-1 do
+    media_track = reaper.GetSelectedTrack(0,i)
+    if media_track ~= nil then
+        reaper.SetMediaTrackInfo_Value(media_track, 'I_RECINPUT', 1)
+        if reaper.GetMediaTrackInfo_Value(media_track, 'I_RECARM') == 0 then
+            reaper.SetMediaTrackInfo_Value(media_track, 'I_RECARM', 1)
+        else
+            reaper.SetMediaTrackInfo_Value(media_track, 'I_RECARM', 0)
+        end
+    end
+end
